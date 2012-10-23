b22bcc5b085d2f84142242a33ae7a2b268e47ac8
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2012-10-23 16:54:25 +0200

    tweaks

--- EventListener/ConfigureMenuChildrenListener.php
+++ EventListener/ConfigureMenuChildrenListener.php
@@ -14,19 +14,15 @@ class ConfigureMenuChildrenListener
 
     public function onMenuChildrenConfigure(ConfigureMenuChildrenEvent $event)
     {
-        $menu = $event->getMenu();
-        $factory = $event->getFactory();
-
-        if ('Settings' == $menu->getName()) {
-            $usersMenu = $menu->addChild($factory->createItem('Users', array('route' => 'KunstmaanAdminBundle_settings_users')));
+        if ('Settings' == $event->getMenu()->getName()) {
+            $usersMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Users', array('route' => 'KunstmaanAdminBundle_settings_users')));
             $usersMenu->setAttribute('rel', 'Users');
-            $groupsMenu = $menu->addChild($factory->createItem('Groups', array('route' => 'KunstmaanAdminBundle_settings_groups')));
+            $groupsMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Groups', array('route' => 'KunstmaanAdminBundle_settings_groups')));
             $groupsMenu->setAttribute('rel', 'Groups');
-            $rolesMenu = $menu->addChild($factory->createItem('Roles', array('route' => 'KunstmaanAdminBundle_settings_roles')));
+            $rolesMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Roles', array('route' => 'KunstmaanAdminBundle_settings_roles')));
             $rolesMenu->setAttribute('rel', 'Roles');
-            $logsMenu = $menu->addChild($factory->createItem('Logs', array('route' => 'KunstmaanAdminBundle_settings_logs')));
+            $logsMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Logs', array('route' => 'KunstmaanAdminBundle_settings_logs')));
             $logsMenu->setAttribute('rel', 'Logs');
-        } else if ('Modules' == $menu->getName()) {
         }
     }
 }

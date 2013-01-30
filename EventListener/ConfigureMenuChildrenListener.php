afb9ba201f576dab2e3a5ece39ba68e36bc1e62b
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-01-30 11:53:44 +0100

    add children to Roles and Groups to fix menu in edit screens

--- EventListener/ConfigureMenuChildrenListener.php
+++ EventListener/ConfigureMenuChildrenListener.php
@@ -34,6 +34,7 @@ class ConfigureMenuChildrenListener
         if ('Settings' == $event->getMenu()->getName()) {
             $usersMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Users', array('route' => 'KunstmaanAdminBundle_settings_users')));
             $usersMenu->setAttribute('rel', 'Users');
+            $usersMenu->setDisplayChildren(false);
             {
                 $request = $this->request;
                 $currentId = $request->get('id');
@@ -43,8 +44,24 @@ class ConfigureMenuChildrenListener
             }
             $groupsMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Groups', array('route' => 'KunstmaanAdminBundle_settings_groups')));
             $groupsMenu->setAttribute('rel', 'Groups');
+            $groupsMenu->setDisplayChildren(false);
+            {
+                $request = $this->request;
+                $currentId = $request->get('id');
+                $addMenu = $groupsMenu->addChild($event->getFactory()->createItem('Add', array('route' => 'KunstmaanAdminBundle_settings_groups_add')));
+                $editMenu = $groupsMenu->addChild($event->getFactory()->createItem('Edit', array('route' => 'KunstmaanAdminBundle_settings_groups_edit', 'routeParameters' => array('id' =>  $currentId))));
+                $deleteMenu = $groupsMenu->addChild($event->getFactory()->createItem('Delete', array('route' => 'KunstmaanAdminBundle_settings_groups_delete', 'routeParameters' => array('id' =>  $currentId))));
+            }
             $rolesMenu = $event->getMenu()->addChild($event->getFactory()->createItem('Roles', array('route' => 'KunstmaanAdminBundle_settings_roles')));
             $rolesMenu->setAttribute('rel', 'Roles');
+            $rolesMenu->setDisplayChildren(false);
+            {
+                $request = $this->request;
+                $currentId = $request->get('id');
+                $addMenu = $rolesMenu->addChild($event->getFactory()->createItem('Add', array('route' => 'KunstmaanAdminBundle_settings_roles_add')));
+                $editMenu = $rolesMenu->addChild($event->getFactory()->createItem('Edit', array('route' => 'KunstmaanAdminBundle_settings_roles_edit', 'routeParameters' => array('id' =>  $currentId))));
+                $deleteMenu = $rolesMenu->addChild($event->getFactory()->createItem('Delete', array('route' => 'KunstmaanAdminBundle_settings_roles_delete', 'routeParameters' => array('id' =>  $currentId))));
+            }
         }
     }
 }

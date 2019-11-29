acrobat/KunstmaanBundlesCMS

[AllBundles] Switch to twig namespaced path notation

src/Kunstmaan/AdminBundle/Twig/TabsTwigExtension.php
@@ -31,7 +31,7 @@ class TabsTwigExtension extends Twig_Extension
      *
      * @return string
      */
-    public function renderWidget(Twig_Environment $env, TabPane $tabPane, $options = array(), $template = 'KunstmaanAdminBundle:TabsTwigExtension:widget.html.twig')
+    public function renderWidget(Twig_Environment $env, TabPane $tabPane, $options = array(), $template = '@KunstmaanAdmin/TabsTwigExtension/widget.html.twig')
     {
         $template = $env->loadTemplate($template);
 

Kunstmaan/KunstmaanAdminBundle

Twig/TabsTwigExtension.php
@@ -32,7 +32,7 @@ class TabsTwigExtension extends AbstractExtension
      *
      * @return string
      */
-    public function renderWidget(Environment $env, TabPane $tabPane, $options = array(), $template = 'KunstmaanAdminBundle:TabsTwigExtension:widget.html.twig')
+    public function renderWidget(Environment $env, TabPane $tabPane, $options = array(), $template = '@KunstmaanAdmin/TabsTwigExtension/widget.html.twig')
     {
         $template = $env->loadTemplate($template);
 